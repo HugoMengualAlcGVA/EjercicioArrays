736cb0a75230c9695e4b2f0223f2848da58fe660
Hugo <hugome2307@gmail.com>
2025-09-28 02:02:54 +0200

    Acabado hasta alumnosCurso()

--- src/app/components/editor-alumnos/editor-alumnos.ts
+++ src/app/components/editor-alumnos/editor-alumnos.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { ALUMNOS } from '../../Modelos/alumnos';
+import { ALUMNOS } from '../../models/alumnos';
 
 @Component({
   selector: 'app-editor-alumnos',
@@ -9,17 +9,71 @@ import { ALUMNOS } from '../../Modelos/alumnos';
   styleUrl: './editor-alumnos.css'
 })
 export class EditorAlumnos {
-  alumnos = ALUMNOS
-  nombreAlumno: string = ""
+  alumnos = ALUMNOS;
 
-  readNombreAlumno(){
-    var answer = prompt("Nombre del alumno")
-    answer != null && answer !=""? this.nombreAlumno=answer : alert("No se introdujo ningun nombre")
+  readAnswer(p: string): string{
+    let answer = prompt(p);
+    if(answer != null && answer !="") return answer; else{
+      alert("No se introdujo ningun nombre");
+      return ""
+    } 
   }
 
   verNotasAlumno(){
-    var notaAlumno
-    this.readNombreAlumno()
-    //notaAlumno = this.alumnos.find(a=>a.nombre==this.nombreAlumno)?.calificaciones
+    let nombreAlumno: string = this.readAnswer("Inserta el nombre del alumno");
+    if (nombreAlumno!=""){
+      let alumno = this.alumnos.find(a=>a.nombre==nombreAlumno);
+      if(alumno!=undefined){
+        let maxNota: number = 0;
+        let asiganaturaMaxNota: string = "";
+        let minNota: number = 10;
+        let asiganaturaMinNota: string = "";
+        let mediaNota: number = 0;
+
+        alumno.calificaciones.forEach(n => {
+          mediaNota += n.nota
+          if (maxNota < n.nota){
+            maxNota = n.nota;
+            asiganaturaMaxNota = n.asignatura;
+          } 
+          if (minNota > n.nota){
+            minNota = n.nota
+            asiganaturaMinNota = n.asignatura
+          }
+        });
+        mediaNota = mediaNota/alumno.calificaciones.length
+        console.log(
+          "Nota media: "+mediaNota+" | Mejor nota: "+asiganaturaMaxNota+", "+maxNota+
+          " | Peor nota: "+asiganaturaMinNota+", "+minNota
+        );
+      }else console.log("El alumno no existe");
+    }
+  }
+
+  subeUnPunto(){
+    this.alumnos.forEach(a => {
+      a.calificaciones.forEach(c => {
+        if (c.nota<10) c.nota++
+      });
+    });
+    console.log(this.alumnos)
+  }
+
+  alumnosCiudad(){
+    let nombreCiudad: string = this.readAnswer("Inserta el nombre de la ciudad");
+    let listaAlumnos: string[] = [];
+    if(nombreCiudad!=""){
+      this.alumnos.filter(a => a.ciudad==nombreCiudad).forEach(a => listaAlumnos.push(a.nombre))
+      console.log(listaAlumnos);
+    }
+  }
+
+  alumnosCurso(){
+    let curso: string = this.readAnswer("Curso:");
+    let listaAlumnos: string[] = [];
+    if(curso!=""){
+      this.alumnos.filter(a => a.codigo.split("-")[2]==curso).forEach(a => listaAlumnos.push(a.nombre))
+      console.log(listaAlumnos);
+    }
   }
 }
